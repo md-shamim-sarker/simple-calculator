69de32c95c04a0e9b76b6b722db6763e46100527
shamimspro <shamimspro@gmail.com>
2022-08-18 00:10:39 +0600

    first commit

---
+++ calc.js
@@ -0,0 +1,17 @@
+function display(val) {
+    document.getElementById('result').value += val;
+    return val;
+}
+
+function solve() {
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+// https://www.simplilearn.com/tutorials/javascript-tutorial/calculator-in-javascript
